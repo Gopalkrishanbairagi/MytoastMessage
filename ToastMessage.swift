28c1c7bf476fe3ecd848ba9405804a1f858a9922
Gopal krishan <gplkrishan727@gmail.com>
2018-04-17 11:16:59 +0530

    Create ToastMessage.swift

---
+++ ToastMessage.swift
@@ -0,0 +1,70 @@
+extension UIViewController {    
+    func toastMessage(_ message: String){
+        guard let window = UIApplication.shared.keyWindow else {return}
+        let messageLbl = UILabel()
+        messageLbl.text = message
+        messageLbl.textAlignment = .center
+        messageLbl.font = UIFont.systemFont(ofSize: 12)
+        messageLbl.textColor = .white
+        messageLbl.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        
+        let textSize:CGSize = messageLbl.intrinsicContentSize
+        let labelWidth = min(textSize.width, window.frame.width - 40)
+        
+        messageLbl.frame = CGRect(x: 20, y: window.frame.height - 90, width: labelWidth + 30, height: textSize.height + 20)
+        messageLbl.center.x = window.center.x
+        messageLbl.layer.cornerRadius = messageLbl.frame.height/2
+        messageLbl.layer.masksToBounds = true
+        window.addSubview(messageLbl)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            
+        UIView.animate(withDuration: 1, animations: {
+            messageLbl.alpha = 0
+        }) { (_) in
+            messageLbl.removeFromSuperview()
+        }
+        }
+    }
+}
+
+
+// Get ViewController from tableViewCell Class
+extension UITableViewCell {
+    var viewControllerForTableView : UIViewController?{
+        return ((self.superview as? UITableView)?.delegate as? UIViewController)
+    }
+}
+
+// Give Color with Hexadecimal code
+
+extension UIColor {
+    convenience init(hex: UInt32, alpha: CGFloat) {
+        let red = CGFloat((hex & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((hex & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(hex & 0xFF)/256.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
+// Create textfeild with Padding
+class TextFieldwithPadding: UITextField {
+    
+    open var padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 5);
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    func placeholderWithDefaultColorColor(){
+        self.attributedPlaceholder = NSAttributedString.init(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor : UIColor.init(hex: 0x6E6E6E, alpha: 1)])
+    }
+}
